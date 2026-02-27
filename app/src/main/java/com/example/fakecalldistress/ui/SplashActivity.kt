bd29e90a61f01fa3f80f18a7c571ceadcdb419cf
Soham_patil <sohampatil0000001@gmail.com>
2026-02-28 02:43:27 +0530

    refactor: remove pin system completely

--- app/src/main/java/com/example/fakecalldistress/ui/SplashActivity.kt
+++ app/src/main/java/com/example/fakecalldistress/ui/SplashActivity.kt
@@ -6,16 +6,15 @@ import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fakecalldistress.R
-import com.example.fakecalldistress.ui.auth.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // Delay for 2 seconds then go to Login
+        // Delay for 2 seconds then go to Main Activity
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }, 2000)
     }
